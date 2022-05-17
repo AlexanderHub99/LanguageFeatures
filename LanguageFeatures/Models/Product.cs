f770d9af62964f99c8e82b44eaabe02cc9f7ed6b
Sasha1448 <agames1448@gmail.com>
2022-05-17 23:06:15 +0300

    [Add Model] Product

---
+++ LanguageFeatures/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Person { get; set; }
+
+        public static Product[] GetProduct()
+        {
+            Product kayak = new Product{ Name = "kayak", Person = 276m};
+            Product lifejacket = new Product{ Name = "lifejacket", Person = 48.95m};
+
+            return new Product[] {kayak, lifejacket, null};
+        }
+    }
+}
+
