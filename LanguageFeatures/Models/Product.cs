aec241d201b605d1814d5d338ebab44e3e67018d
Sasha1448 <agames1448@gmail.com>
2022-05-18 23:36:35 +0300

    [Add] adding nesting

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -3,14 +3,16 @@
     public class Product
     {
         public string Name { get; set; }
-        public decimal Person { get; set; }
+        public decimal? Person { get; set; }
+        public Product  Related { get; set; } //Добавил вложенности для усложнения 
 
         public static Product[] GetProduct()
         {
             Product kayak = new Product{ Name = "kayak", Person = 276m};
             Product lifejacket = new Product{ Name = "lifejacket", Person = 48.95m};
+            kayak.Related = lifejacket;
 
-            return new Product[] {kayak, lifejacket, null};
+            return new Product[] {kayak, lifejacket, null};//Добавил нулевую ссылку для усложнения и реализации проверки на null
         }
     }
 }
