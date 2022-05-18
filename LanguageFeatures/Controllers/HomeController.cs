40587e50e3805408cfa2fb0ab793b3ff0c1fb9e0
Sasha1448 <agames1448@gmail.com>
2022-05-18 23:46:58 +0300

    [Added] Combining null - conditional operation and the operation of union with null

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -10,9 +10,9 @@ public class HomeController: Controller
         List<string> results = new List<string>();
         foreach (Product product in Product.GetProduct())
         {
-            string name = product?.Name!;                 //проверка на null в избежании NullReferenceException
-            decimal? price = product?.Person;             //проверка на null в избежании NullReferenceException
-            string relatedName = product?.Related?.Name!; //проверка на null в избежании NullReferenceException
+            string name = product?.Name ?? "<No Name>";                 //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
+            decimal? price = product?.Person ?? 0;              //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
+            string relatedName = product?.Related?.Name ?? "<No Name>";   //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
             results.Add($"Name:  {name} ,  Price: {price},  Related: {relatedName}");
         }
         return View(results);
