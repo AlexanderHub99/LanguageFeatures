9bedfd24b2edc5d9a6b64a3f3c2fbba0c4bf31bc
Sasha1448 <agames1448@gmail.com>
2022-05-19 17:58:14 +0300

    [Added] default values

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -3,19 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LanguageFeatures.Controllers
 {
-public class HomeController: Controller
-{
-    public ViewResult Index()
+    public class HomeController : Controller
     {
-        List<string> results = new List<string>();
-        foreach (Product product in Product.GetProduct())
+        public ViewResult Index()
         {
-            string name = product?.Name ?? "<No Name>";                 //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
-            decimal? price = product?.Person ?? 0;              //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
-            string relatedName = product?.Related?.Name ?? "<No Name>";   //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
-            results.Add($"Name:  {name} ,  Price: {price},  Related: {relatedName}");
+            var results = new List<string>();
+            foreach (Product product in Product.GetProduct())
+            {
+                string name = product?.Name ?? "<No Name>"; //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
+                decimal? price = product?.Person ?? 0; //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
+                string relatedName = product?.Related?.Name ?? "<No Name>"; //проверка на null в избежание NullReferenceException и добавление дефолтного значения .
+                results.Add($"Name:  {name} ,  Price: {price:C2},  Related: {relatedName}");
+            }
+            return View(results);
         }
-        return View(results);
     }
-}
 }
--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -2,14 +2,21 @@
 {
     public class Product
     {
-        public string Name { get; set; }
+        public Product(bool stock = true)
+        {
+            InStock = InStock;
+        }
+        public string Name { get; set; } = null!;
+        public string Category { get; set; } = "Watersports";
         public decimal? Person { get; set; }
-        public Product  Related { get; set; } //Добавил вложенности для усложнения 
+        public Product  Related { get; set; } = null!; //Добавил вложенности для усложнения 
+
+        public bool InStock { get; } 
 
         public static Product[] GetProduct()
         {
-            Product kayak = new Product{ Name = "kayak", Person = 276m};
-            Product lifejacket = new Product{ Name = "lifejacket", Person = 48.95m};
+            Product kayak = new Product{ Name = "kayak", Category = "Water Craft", Person = 276m,};
+            Product lifejacket = new Product(false){ Name = "lifejacket", Person = 48.95m};
             kayak.Related = lifejacket;
 
             return new Product[] {kayak, lifejacket, null};//Добавил нулевую ссылку для усложнения и реализации проверки на null
